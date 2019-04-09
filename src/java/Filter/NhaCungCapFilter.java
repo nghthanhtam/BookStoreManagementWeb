269b379b14b70884a6f7047e6b794aafd9831075
Dang Minh Tien <dangminhtien06@gmail.com>
2019-04-09 12:11:09 +0700

    Add filter Thành viên, nhà cung cấp, phí ship

--- src/java/Filter/NhaCungCapFilter.java
+++ src/java/Filter/NhaCungCapFilter.java
@@ -29,12 +29,12 @@ import javax.servlet.http.HttpSession;
  *
  * @author MITICC06
  */
-public class PhanQuyenFilter implements Filter {
-    
-    public void doFilter(ServletRequest request, ServletResponse response,
+public class NhaCungCapFilter implements Filter {
+        public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
         
+         
         HttpServletRequest req = (HttpServletRequest) request;
         HttpSession session = req.getSession();
         
@@ -49,13 +49,13 @@ public class PhanQuyenFilter implements Filter {
             Logger.getLogger(PhanQuyenFilter.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        if (phanQuyen!=null && phanQuyen.getQlPhanQuyen() != 0)
+        if (phanQuyen!=null && phanQuyen.getQlNhaCungCap()!= 0)
         {
             chain.doFilter(request, response);// vượt qua filter
         }
         else
         {            
-            request.setAttribute("tenChucNang","Phân Quyền");
+            request.setAttribute("tenChucNang","Nhà cung cấp");
 
             
             request.getRequestDispatcher("/admin/chantruycap.jsp").forward(request, response);
@@ -72,5 +72,4 @@ public class PhanQuyenFilter implements Filter {
        
     }
 
-  
 }
--- src/java/Filter/PhiShipFilter.java
+++ src/java/Filter/PhiShipFilter.java
@@ -29,9 +29,9 @@ import javax.servlet.http.HttpSession;
  *
  * @author MITICC06
  */
-public class PhanQuyenFilter implements Filter {
+public class PhiShipFilter implements Filter {
     
-    public void doFilter(ServletRequest request, ServletResponse response,
+        public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
         
@@ -49,13 +49,13 @@ public class PhanQuyenFilter implements Filter {
             Logger.getLogger(PhanQuyenFilter.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        if (phanQuyen!=null && phanQuyen.getQlPhanQuyen() != 0)
+        if (phanQuyen!=null && phanQuyen.getQlPhiShip()!= 0)
         {
             chain.doFilter(request, response);// vượt qua filter
         }
         else
         {            
-            request.setAttribute("tenChucNang","Phân Quyền");
+            request.setAttribute("tenChucNang","Phí ship");
 
             
             request.getRequestDispatcher("/admin/chantruycap.jsp").forward(request, response);
@@ -71,6 +71,4 @@ public class PhanQuyenFilter implements Filter {
     public void init(FilterConfig filterConfig) {        
        
     }
-
-  
 }
